dc44d84fb5786eceb9855499c58f340576c75deb
Ubuntu <ubuntu@ip-172-31-41-50.eu-west-1.compute.internal>
2023-12-20 13:09:04 +0000

    Commit

--- proyectos/claves_ssh/locals.tf
+++ proyectos/claves_ssh/locals.tf
@@ -1,8 +1,27 @@
 locals {
-    se_generan_claves   = si no existen ficheros previos de claves o cuando se fuerza
+    directorio_ficheros_claves_acabando_en_barra    = ( endswith(var.directorio_ficheros_claves, "/") 
+                                                         ? var.directorio_ficheros_claves
+                                                         : "${var.directorio_ficheros_claves}/" )
+                                                             
+    ruta_del_fichero_clave_publica_pem              = "${local.directorio_ficheros_claves_acabando_en_barra}public_key.pem"
+    ruta_del_fichero_clave_publica_openssh          = "${local.directorio_ficheros_claves_acabando_en_barra}public_key.openssh"
+    ruta_del_fichero_clave_privada_pem              = "${local.directorio_ficheros_claves_acabando_en_barra}private_key.pem"
+    ruta_del_fichero_clave_privada_openssh          = "${local.directorio_ficheros_claves_acabando_en_barra}private_key.openssh"
     
-    contenido_del_fichero_clave_privada_openssh     =
-    contenido_del_fichero_clave_privada_pem         =
-    contenido_del_fichero_clave_publica_openssh     =
-    contenido_del_fichero_clave_publica_pem         = file(ruta)
+    existe_el_fichero_clave_publica_pem             = fileexists( local.ruta_del_fichero_clave_publica_pem     )
+    existe_el_fichero_clave_publica_openssh         = fileexists( local.ruta_del_fichero_clave_publica_openssh )
+    existe_el_fichero_clave_privada_pem             = fileexists( local.ruta_del_fichero_clave_privada_pem     )
+    existe_el_fichero_clave_privada_openssh         = fileexists( local.ruta_del_fichero_clave_privada_openssh )
+
+    existen_ficheros_de_claves                      = ( local.existe_el_fichero_clave_publica_pem
+                                                        && local.existe_el_fichero_clave_publica_openssh
+                                                        && local.existe_el_fichero_clave_privada_pem
+                                                        && local.existe_el_fichero_clave_privada_openssh )
+    
+    se_generan_claves                               = ! local.existen_ficheros_de_claves || var.forzar_regeneracion
+
+    contenido_del_fichero_clave_publica_pem         = local.existe_el_fichero_clave_publica_pem     ? file(local.ruta_del_fichero_clave_publica_pem)     : null
+    contenido_del_fichero_clave_publica_openssh     = local.existe_el_fichero_clave_publica_openssh ? file(local.ruta_del_fichero_clave_publica_openssh) : null
+    contenido_del_fichero_clave_privada_pem         = local.existe_el_fichero_clave_privada_pem     ? file(local.ruta_del_fichero_clave_privada_pem)     : null
+    contenido_del_fichero_clave_privada_openssh     = local.existe_el_fichero_clave_privada_openssh ? file(local.ruta_del_fichero_clave_privada_openssh) : null
 }
