01b2c0cf51e7003dc9782f8648d114ca49bb9d53
Ubuntu <ubuntu@ip-172-31-41-50.eu-west-1.compute.internal>
2023-12-20 12:45:24 +0000

    Commit

--- proyectos/claves_ssh/locals.tf
+++ proyectos/claves_ssh/locals.tf
@@ -1,8 +1,8 @@
 locals {
-    se_generan_claves   =
+    se_generan_claves   = si no existen ficheros previos de claves o cuando se fuerza
     
     contenido_del_fichero_clave_privada_openssh     =
     contenido_del_fichero_clave_privada_pem         =
     contenido_del_fichero_clave_publica_openssh     =
-    contenido_del_fichero_clave_publica_pem         =
+    contenido_del_fichero_clave_publica_pem         = file(ruta)
 }
